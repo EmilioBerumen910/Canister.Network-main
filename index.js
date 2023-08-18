4e095db06168ad3aefd3bd68bf1e97ec8dd16db6
EmilioBerumen90 <106618441+EmilioBerumen90@users.noreply.github.com>
2023-08-18 19:29:45 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_7KfLw8_Sq4vXgYuU3-aaSmNOt_JiSAU",
+  authDomain: "canister-aa7a2.firebaseapp.com",
+  projectId: "canister-aa7a2",
+  storageBucket: "canister-aa7a2.appspot.com",
+  messagingSenderId: "876684192515",
+  appId: "1:876684192515:web:f389ac9e55431688ebd736",
+  measurementId: "G-GLQZTM66KY"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js";
+
+var storage = firebase.storage();
+
+var storageRef = storage.ref();
+
+var DETACHEDFilm = storageRef.child('Films/DETACHED SHORT FILM.mp4');
+var BirthDayCakeFilm = storageRef.child('Films/Birthday Cake (2022) - Short Film.mp4');
+var TBWEGUFilm = storageRef.child('Films/The Boy that eventually grow up.mp4');
+var VanitasFilm = storageRef.child('Films/VANITAS SHORT FILM.mp4');
+var MasteryJournalMario = storageRef.child('Films/MarioRangel_masteryjournal.mp4');
+var MasteryJournalNatalia = storageRef.child('Films/mastery journal natala gonzalez.mp4');
+var SabrosoFilm=storageRef.child('Films/SABROSO VIDEO.mp4');
+var ReelColorist =storageRef.child('Films/Reel Colorist 2022.mp4');
+
+
+DETACHEDFilm.getDownloadURL().then(function(url) {
+var videoElement = document.getElementById('DetachedFilm');
+videoElement.src = url;
+}).catch(function(error) {
+console.log(error);
+});
+
+BirthDayCakeFilm.getDownloadURL().then(function(url) {
+  var videoElement = document.getElementById('BirthDayCakeFilm');
+  videoElement.src = url;
+  }).catch(function(error) {
+  console.log(error);
+  });
+
+  TBWEGUFilm.getDownloadURL().then(function(url) {
+    var videoElement = document.getElementById('TBWEGUFilm');
+    videoElement.src = url;
+    }).catch(function(error) {
+    console.log(error);
+    });
+
+  VanitasFilm.getDownloadURL().then(function(url) {
+    var videoElement = document.getElementById('VanitasFilm');
+    videoElement.src = url;
+    }).catch(function(error) {
+    console.log(error);
+    });
+
+    
+    MasteryJournalMario.getDownloadURL().then(function(url) {
+    var videoElement = document.getElementById('MasteryJournalMario');
+    videoElement.src = url;
+    }).catch(function(error) {
+    console.log(error);
+    });
+
+    MasteryJournalNatalia.getDownloadURL().then(function(url) {
+      var videoElement = document.getElementById('MasteryJournalNatalia');
+      videoElement.src = url;
+      }).catch(function(error) {
+      console.log(error);
+      });
+
+      SabrosoFilm.getDownloadURL().then(function(url) {
+        var videoElement = document.getElementById('SabrosoFilm');
+        videoElement.src = url;
+        }).catch(function(error) {
+        console.log(error);
+        });
+
+        ReelColorist.getDownloadURL().then(function(url) {
+          var videoElement = document.getElementById('ReelColorist');
+          videoElement.src = url;
+          }).catch(function(error) {
+          console.log(error);
+          });
+
